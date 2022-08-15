5d305d7ddd2214ef5847cab90777ac1ff8adbd83
manishchouhan2396 <manishchouhan2396@gmail.com>
2022-08-15 20:45:44 +0530

    youtube

--- src/Conponets/Navbar.jsx
+++ src/Conponets/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import logo from "../Picture/homo-logo.png";
 import { getdata } from '../Redux/action';
-import { Box, Button, Input, Center, Image, Spacer, HStack , Avatar } from "@chakra-ui/react";
+import { Box, Button, Input, Center, Image, Spacer, HStack, Avatar } from "@chakra-ui/react";
 import { BsSearch } from 'react-icons/bs'
 import { Link } from "react-router-dom"
 import profile from "../Picture/manish.jpg"
@@ -39,15 +39,16 @@ const Navbar = () => {
       </Box>
       <Spacer />
       <Box borderColor="black">
-        <HStack  mr={10}>
-          <Input width="600px"  borderRadius={0} border-Color="#fff" type="text" placeholder="Search..." onChange={(e) => setText(e.target.value)} />
-          <Button onClick={handleClick} borderRadius={0} ><BsSearch /></Button>
-          {/* <BsSearch/> */}
+        <HStack mr={10}>
+          <Input width="600px" borderRadius={0} border-Color="#fff" type="text" placeholder="Search..." onChange={(e) => setText(e.target.value)} />
+          <Button onClick={handleClick} borderRadius={0} >
+            <BsSearch />
+          </Button>
         </HStack>
       </Box>
       <Spacer />
       <Box >
-        <Avatar src={profile}/>
+        <Avatar src={profile} />
       </Box>
     </Center>
   )
--- src/Pages/Single.jsx
+++ src/Pages/Single.jsx
@@ -1,8 +1,9 @@
-import { AspectRatio, Box , Heading } from '@chakra-ui/react';
-import React  , {useEffect}from 'react'
+import { AspectRatio, Box, Heading } from '@chakra-ui/react';
+import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 // import { videoData } from "../utils/data"
 import { useSelector } from 'react-redux'
+import {Link} from "react-router-dom"
 
 const Single = () => {
 
@@ -14,12 +15,11 @@ const Single = () => {
 
   console.log(data)
 
-  console.log()
 
 
-  useEffect(()=>{
-    document.title= data[0].snippet.title;
-  },[data])
+  useEffect(() => {
+    document.title = data[0].snippet.title;
+  }, [data])
 
 
 
@@ -28,18 +28,44 @@ const Single = () => {
   // console.log(id)
 
   return (
-    <Box m="2rem">
-      <AspectRatio maxW='50rem' ratio={5}>
-        <iframe
-          title='naruto'
-          src={`https://www.youtube.com/embed/${data[0].id.videoId}`}
-          allowFullScreen
-        />
-        
-      </AspectRatio>
-      <Heading fontSize={30} maxW="50rem" >{data[0].snippet.title}</Heading>
-      
+    <Box>
+      <Box m="3rem">
+        <AspectRatio maxW='50rem' ratio={2}>
+          <iframe
+            title='naruto'
+            src={`https://www.youtube.com/embed/${data[0].id.videoId}`}
+            allowFullScreen
+          />
+
+        </AspectRatio>
+        <Heading fontSize={30} maxW="50rem" >{data[0].snippet.title}</Heading>
+      </Box>
+      <Box pos="absolute" right="5rem" maxW="20rem" top="6rem">
+        {
+          videoData && videoData.map((e) => {
+
+            return (
+
+              <Box key={e.etag} maxW="100%">
+                <Link to={`/${e.id.videoId}`}>
+                  <Box
+                    as="iframe"
+                    w="100%"
+                    src={`https://www.youtube.com/embed/${e.id.videoId}`}
+                  // allowFullScreen
+                  />
+                  <br />
+                  <Heading fontSize={15} >{e.snippet.title}</Heading>
+                </Link>
+              </Box>
+
+            )
+
+          })
+        }
+      </Box>
     </Box>
+
   )
 }
 
